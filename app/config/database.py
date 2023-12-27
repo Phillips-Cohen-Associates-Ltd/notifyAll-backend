2b7661f8cd903e8081638c19444eac64858c5b6e
Lucas Gonzales <lgonzales@phillips-cohen.com>
2023-12-27 14:13:35 -0500

    no message

--- app/config/database.py
+++ app/config/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from ..config.config import settings
 
 # SQLITE_DATABASE_URL = "mysql://root:00000@127.0.0.1/fastapi"
 
 SQLITE_DATABASE_URL = f"mysql+mysqlconnector://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}/{settings.POSTGRES_DB}"
+print(SQLITE_DATABASE_URL)
 # engine = create_engine(
 #     SQLITE_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
 # )
@@ -15,11 +17,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
